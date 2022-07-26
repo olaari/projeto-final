82c908a8ed529fdff90bba3a67bfab13445f2f46
Larissa da Silva de Oliveira <larissasilvaoliveira27@gmail.com>
2022-07-26 17:09:50 -0300

    Add foto na classificação e responsividade menu

--- src/pages/Classificacao/Classificacao.jsx
+++ src/pages/Classificacao/Classificacao.jsx
@@ -1,6 +1,18 @@
-function Classificacao(){
-    return(
-        <h1>Classificação</h1>
+import "./classificacao.css";
+import rotuloAlimento from "../../assets/rotulo-alimento.png";
+
+function Classificacao() {
+    return (
+        <>
+            <div className="main">
+                <img className="rotulo-alimento" src={rotuloAlimento}></img>
+                <div className="background-classificacao"></div>
+            </div>
+            <div>
+                <h1>Classificação dos Alimentos</h1>
+                <h3>Sendo o guia alimentar os alimentos podem ser dividido em: </h3>
+            </div>
+        </>
     )
 
 }
