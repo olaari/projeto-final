08ee8d1a3fcaeaa235750e3d33fdc49d81704b98
Larissa da Silva de Oliveira <larissasilvaoliveira27@gmail.com>
2022-07-26 22:04:09 -0300

    add header classificação

--- src/pages/Classificacao/Classificacao.jsx
+++ src/pages/Classificacao/Classificacao.jsx
@@ -1,6 +1,7 @@
 import "./classificacao.css";
 import rotuloAlimento from "../../assets/rotulo-alimento.png";
 
+
 function Classificacao() {
     return (
         <>
@@ -8,9 +9,23 @@ function Classificacao() {
                 <img className="rotulo-alimento" src={rotuloAlimento}></img>
                 <div className="background-classificacao"></div>
             </div>
-            <div>
+            <div className="main-classificacao">
                 <h1>Classificação dos Alimentos</h1>
-                <h3>Sendo o guia alimentar os alimentos podem ser dividido em: </h3>
+                <p> O guia alimentar classifica os alimentos de acordo com o nível de processamento dos alimentos e podem ser divididos em:</p>
+                <ul className="lista-alimentos">
+                    <li className="categorias-alimentos">Alimentos in natura e minimamente processados.</li>
+                    <li className="categorias-alimentos">Alimentos ingredientes culinários processados.</li>
+                    <li className="categorias-alimentos" >Alimentos processados.</li>
+                    <li className="categorias-alimentos">Alimentos ultraprocessados.</li>
+                </ul>
+
+                <h3>Alimentos in natura e minimamente processados</h3>
+                <p> </p>
+                <h3>Alimentos ingredientes culinários processados</h3>
+                <p></p>
+                <h3>Alimentos processados</h3>
+                <p></p>
+                <h3>Alimentos ultraprocessados</h3>
             </div>
         </>
     )
